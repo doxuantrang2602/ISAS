744da312270ea1feb73c4a894cf8a624da898785
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-03 14:44:14 +0700

    Cập nhật Vigenere Lặp khóa và Tạo bảng giải thích

--- Chương 1 - Mã hóa cổ điển/Vigenere_LapKhoa.py
+++ Chương 1 - Mã hóa cổ điển/Vigenere_LapKhoa.py
@@ -1,22 +1,28 @@
+'''
+Input: M = LOVEISBLINDLOVE
+Key: K = WHENIN
+Tìm Output: C =
+'''
 if __name__ == '__main__':
-    lstApha= ['A','B','C','D','E','F','G','H','I','J','K','L','M',
+    lstAlpha= ['A','B','C','D','E','F','G','H','I','J','K','L','M',
               'N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    M= list(input("Nhập M: "))
-    K= list(input("Nhập K: "))
-
-    Alpha={}
+    M= list(input("Nhập M = "))
+    K= list(input("Nhập K = "))
+    Alpha = {}
     for i in range(0,26):
-        Alpha[lstApha[i]]=i
-    #xu ly K
-    while len(K) < len(M):
-        K += K[:len(M) - len(K)]
+        Alpha[lstAlpha[i]] = i # Ánh xạ chỉ số i cho mỗi kí tự
+    # Xử lý mở rộng K
+    moRong_K = K * (len(M) // len(K)) + K[:len(M) % len(K)]
 
-    C = []
-    for m, k in zip(M, K):
-        c = (Alpha[m] + Alpha[k]) % 26
-        for a, b in Alpha.items():
-            if b == c:
-                C.append(a)
+    C = []  # Output C kết quả
 
-    print("Kết quả của lặp khóa  :  ", *C, sep="")
+    print(f'{"M":<5} | {"K":<5} | {"M index":<8} | {"K index":<8} | {"C index":<8} | {"C"}') # In tiêu đề cho bảng
+
+    for m, k in zip(M, moRong_K):
+        m_index = Alpha[m]  # Chỉ số của ký tự m trong bảng chữ cái
+        k_index = Alpha[k]  # Chỉ số của ký tự k trong bảng chữ cái
+        c_index = (m_index + k_index) % 26  # Chỉ số mới cho ký tự mã hóa
+        C.append(lstAlpha[c_index])
+        print(f'{m:<5} | {k:<5} | {m_index:<8} | {k_index:<8} | {c_index:<8} | {lstAlpha[c_index]}')
+    print("Kết quả của Vigenere lặp khóa: ",''.join(C), sep="")
 
