2c77eae037fecfbbacbade32574d59d0c1735482
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-03 16:34:32 +0700

    Cập nhật chia hàm LogaritRoiRac

--- Chương 2 - Modulo/Logarit_roi_rac.py
+++ Chương 2 - Modulo/Logarit_roi_rac.py
@@ -1,28 +1,33 @@
+'''
+Đề bài: TÌM LOGARITHM RỜI RẠC CỦA SỐ b VỚI CƠ SỐ a (mod n), 𝒌 = 𝐥𝐨𝐠𝒂 𝒃 (𝒎𝒐𝒅 𝒏).
+Input: a = 6; b = 5; n = 13
+=> Tìm Output: k =
+'''
+
 import math
-def Tinh_Mod(a,i, n):
-    return pow(a,i, n)
-def euler_phi(n):
-    result = n
-    p = 2
-    while p * p <= n:
-        if n % p == 0:  #
-            while n % p == 0:
-                n //= p
-            result -= result // p
-        p += 1
-    if n > 1:
-        result -= result // n
-    return result
+def Tinh_Mod(a, i, n):
+    return pow(a,i,n) # Tính a^i mod n
 
-if __name__ == '__main__':
-    a=int(input("Nhập a:"))
-    b=int(input("Nhập b:"))
-    n=int(input("Nhập n: "))
+def euler_Phi(n):
+    res = 0 # Biến res để đếm số lượng nguyên tố cùng nhau với n
+    for i in range(1, n):
+        if math.gcd(i, n) == 1:
+            res += 1 # Nếu i và n nguyên tố cùng nhau thì tăng res lên 1
+    return res
 
-    for i in range(1,euler_phi(n)):
-        p= Tinh_Mod(a,i,n)
-        if p==b:
-            print("log{} {} (mod {}) = {}".format( a,b,n,i))
+def logaritRoiRac(a, b, n):
+    for i in range(1, euler_Phi(n)):
+        p = Tinh_Mod(a, i, n)
+        if p == b: # Kiểm tra xem a^i mod n có bằng b không => loga b (mod n)=i
+            print("=> log{} {} (mod {}) = {}".format(a,b,n,i))
             break
     else:
-        print("Khong ton tai ")
+        print("Khong ton tai")
+
+if __name__ == "__main__":
+    a = int(input("Nhập a = "))
+    b = int(input("Nhập b = "))
+    n = int(input("Nhập n = "))
+    logaritRoiRac(a,b,n)
+
+
