06b95009a3f7a7054c86400695f996976aea6105
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-03 15:11:33 +0700

    Hoàn thành Vigenere Lặp khóa, AutoKey và tạo bảng giải thích

--- Chương 1 - Mã hóa cổ điển/Vigenere_AutoKey.py
+++ Chương 1 - Mã hóa cổ điển/Vigenere_AutoKey.py
@@ -1,20 +1,29 @@
-lstApha=['A','B','C','D','E','F','G','H','I','J','K','L','M',
-              'N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-M= list(input("Nhập M: "))
-K= list(input("Nhập K: "))
-Alpha={}
-for i in range(0,26):
-    Alpha[lstApha[i]]=i
+'''
+Input: M = LOVEISBLINDLOVE
+Key: K = WHENIN
+=> Tìm Output: C =
+'''
+if __name__ == "__main__":
+    lstAlpha=['A','B','C','D','E','F','G','H','I','J','K','L','M',
+                  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    M= list(input("Nhập M = "))
+    K= list(input("Nhập K = "))
+    Alpha={}
+    for i in range(0,26):
+        Alpha[lstAlpha[i]]=i
 
-while len(K) < len(M):
-    K += M[:len(M) - len(K)]
+    while len(K) < len(M): # Lặp khi độ dài khóa K < M
+        K += M[:len(M) - len(K)] # Mở rộng K bằng cách thêm vào đầu của M cho đến khi len(K) = len(M)
 
-C = []
-for m, k in zip(M, K):
-    c = (Alpha[m] + Alpha[k]) % 26
-    for a, b in Alpha.items():
-        if b == c:
-            C.append(a)
-
-print("Kết của của AutoKey: ", *C, sep="")
+    C = []
+    print("======================================================")
+    print(f'| {"M":<5} | {"K":<5} | {"M index":<8} | {"K index":<8} | {"C index":<8} | {"C"} |')  # In tiêu đề cho bảng
+    print("======================================================")
+    for m, k in zip(M, K):
+        c = (Alpha[m] + Alpha[k]) % 26
+        for a, b in Alpha.items():
+            if b == c:
+                C.append(a)
+        print(f'| {m:<5} | {k:<5} | {Alpha[m]:<8} | {Alpha[k]:<8} | {c:<8} | {lstAlpha[c]} |')
+    print("+> Kết của của Vigenere AutoKey: ",''.join(C), sep="")
 
--- Chương 1 - Mã hóa cổ điển/Vigenere_LapKhoa.py
+++ Chương 1 - Mã hóa cổ điển/Vigenere_LapKhoa.py
@@ -16,13 +16,15 @@ if __name__ == '__main__':
 
     C = []  # Output C kết quả
 
-    print(f'{"M":<5} | {"K":<5} | {"M index":<8} | {"K index":<8} | {"C index":<8} | {"C"}') # In tiêu đề cho bảng
+    print("======================================================")
+    print(f'| {"M":<5} | {"K":<5} | {"M index":<8} | {"K index":<8} | {"C index":<8} | {"C"} |') # In tiêu đề cho bảng
+    print("======================================================")
 
     for m, k in zip(M, moRong_K):
         m_index = Alpha[m]  # Chỉ số của ký tự m trong bảng chữ cái
         k_index = Alpha[k]  # Chỉ số của ký tự k trong bảng chữ cái
         c_index = (m_index + k_index) % 26  # Chỉ số mới cho ký tự mã hóa
         C.append(lstAlpha[c_index])
-        print(f'{m:<5} | {k:<5} | {m_index:<8} | {k_index:<8} | {c_index:<8} | {lstAlpha[c_index]}')
+        print(f'| {m:<5} | {k:<5} | {m_index:<8} | {k_index:<8} | {c_index:<8} | {lstAlpha[c_index]} |')
     print("Kết quả của Vigenere lặp khóa: ",''.join(C), sep="")
 
