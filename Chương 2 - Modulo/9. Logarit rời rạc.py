3a0cf3be8bf442cd5c7c2dfe8d6fd313881af581
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-10 20:39:27 +0700

    Update new Logarit rời rạc

--- Chương 2 - Modulo/9. Logarit rời rạc.py
+++ Chương 2 - Modulo/9. Logarit rời rạc.py
@@ -3,35 +3,18 @@
 Input: a = 6; b = 5; n = 13
 => Tìm Output: k =
 '''
-
-import math
-def Tinh_Mod(a, i, n):
-    return pow(a,i,n) # Tính a^i mod n
-
-def euler_Phi(n):
-    res = 0 # Biến res để đếm số lượng nguyên tố cùng nhau với n
-    for i in range(1, n):
-        if math.gcd(i, n) == 1:
-            res += 1 # Nếu i và n nguyên tố cùng nhau thì tăng res lên 1
-    return res
-
-def logaritRoiRac(a, b, n):
-    print("=========")
-    print("| i | p |")
-    print("=========")
-    for i in range(1, euler_Phi(n)):
-        p = Tinh_Mod(a, i, n)
-        print(f"|{i:<3}|{p:<3}|")
-        if p == b: # Kiểm tra xem a^i mod n có bằng b không => loga b (mod n)=i
-            print("=> log{} {} (mod {}) = {}".format(a,b,n,i))
-            break
-    else:
-        print("Khong ton tai")
-
 if __name__ == "__main__":
-    a = int(input("Nhập a = "))
-    b = int(input("Nhập b = "))
-    n = int(input("Nhập n = "))
-    logaritRoiRac(a,b,n)
-
-
+    a = 6
+    b = 5
+    n = 13
+    res = 0
+    kt = False
+    for i in range(n):
+        if pow(a, i, n) == b:
+           res = i
+           kt = True
+           break
+    if kt == True:
+        print(f"log{a} {b} (mod {n}) = {res}")
+    else:
+        print("Không có giá trị !")
