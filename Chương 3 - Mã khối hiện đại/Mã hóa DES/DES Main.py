a7ac0a2b8cb5672221166ef3b7653dc58061ae38
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-10 23:03:34 +0700

    Clean code Mã hóa DES

--- Chương 3 - Mã khối hiện đại/Mã hóa DES/DES Main.py
+++ Chương 3 - Mã khối hiện đại/Mã hóa DES/DES Main.py
@@ -5,18 +5,17 @@ Output: y – chuỗi số 64 bít được mã hóa từ x theo thuật toán D
 '''
 from DES_MaHoa import *
 
-FP_table = [
-    40, 8, 48, 16, 56, 24, 64, 32,
-    39, 7, 47, 15, 55, 23, 63, 31,
-    38, 6, 46, 14, 54, 22, 62, 30,
-    37, 5, 45, 13, 53, 21, 61, 29,
-    36, 4, 44, 12, 52, 20, 60, 28,
-    35, 3, 43, 11, 51, 19, 59, 27,
-    34, 2, 42, 10, 50, 18, 58, 26,
-    33, 1, 41, 9, 49, 17, 57, 25
-]
-
 def FP(x):
+    FP_table = [
+        40, 8, 48, 16, 56, 24, 64, 32,
+        39, 7, 47, 15, 55, 23, 63, 31,
+        38, 6, 46, 14, 54, 22, 62, 30,
+        37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 52, 20, 60, 28,
+        35, 3, 43, 11, 51, 19, 59, 27,
+        34, 2, 42, 10, 50, 18, 58, 26,
+        33, 1, 41, 9, 49, 17, 57, 25
+    ]
     res = ""
     for i in FP_table:
         res += x[i-1]
@@ -35,17 +34,20 @@ def DES(x, k):
        print(f"Khóa {i+1} = {Ks[i]}")
     IP_M = IP(M)
     L0, R0 = SPLIT_KEY(IP_M)
+    L, R = [], []
+    L.append(L0)
+    R.append(R0)
     print(f"Vòng 0: L = {L0}, R = {R0}")
     for i in range(16):
-        temp = R0
-        ER0 = E(R0)
-        A = XOR(ER0, Ks[i])
+        ER = E(R[i])
+        A = XOR(ER, Ks[i])
         B = SUB(A)
         F = P(B)
-        R0 = XOR(F, L0)
-        L0 = temp
-        print(f"Vòng {i+1}: L = {L0}, R = {R0}")
-    y = FP(R0+L0)
+        R_res = XOR(F, L[i])
+        L.append(R[i])
+        R.append(R_res)
+        print(f"Vòng {i+1}: L = {L[i+1]}, R = {R[i+1]}")
+    y = FP(R[16]+L[16])
     return y
 def binToHex(s):
     strHex = {
@@ -69,4 +71,5 @@ if __name__ == '__main__':
 '''
 K = 03756CD378146EC7
 M = 66581B2AE5B0BD6D
+Output: F20CA5AE288E3903
 '''
--- Chương 3 - Mã khối hiện đại/Mã hóa DES/DES_SinhKhoa.py
+++ Chương 3 - Mã khối hiện đại/Mã hóa DES/DES_SinhKhoa.py
@@ -14,16 +14,6 @@ Input: C, D - chuỗi số 28 bit, s – số nguyên dương < 28
 Output: Ks – chuỗi số 48 bit là hoán vị của C, D theo ma trận PC2.
 '''
 
-PC1_table = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18,
-           10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36,
-           63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22,
-           14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4]
-
-PC2_table = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
-           23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
-           41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
-           44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
-
 shift = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 
 def hexToBin(s):
@@ -39,6 +29,16 @@ def hexToBin(s):
     return bin
 
 def PC1(K):
+    PC1_table = [
+        57, 49, 41, 33, 25, 17, 9,
+        1, 58, 50, 42, 34, 26, 18,
+        10, 2, 59, 51, 43, 35, 27,
+        19, 11, 3, 60, 52, 44, 36,
+        63, 55, 47, 39, 31, 23, 15,
+        7, 62, 54, 46, 38, 30, 22,
+        14, 6, 61, 53, 45, 37, 29,
+        21, 13, 5, 28, 20, 12, 4
+    ]
     K1 = ""
     for x in PC1_table:
         K1 += K[x-1]
@@ -55,6 +55,14 @@ def ShiftLeft(x, s):
     return x[bitRotated:] + x[:bitRotated]
 
 def PC2(C,D):
+    PC2_table = [
+        14, 17, 11, 24, 1, 5, 3, 28,
+        15, 6, 21, 10, 23, 19, 12, 4,
+        26, 8, 16, 7, 27, 20, 13, 2,
+        41, 52, 31, 37, 47, 55, 30, 40,
+        51, 45, 33, 48, 44, 49, 39, 56,
+        34, 53, 46, 42, 50, 36, 29, 32
+    ]
     K= C+D
     K1 = ""
     for x in PC2_table:
@@ -62,17 +70,16 @@ def PC2(C,D):
     return K1
 
 if __name__ == '__main__':
-    K= '03756CD378146EC7'
-    K= hexToBin(K)
-    K1= PC1(K)
-    print("=> Hoán vị PC1: ",K1, sep="")
-    C,D= SPLIT_KEY(K1)
-    print("C= ",C)
-    print("D= ",D)
-    C=ShiftLeft(C, 1)
-    D=ShiftLeft(D, 1)
-    print("Dịch vòng trái 1 bit của C= ",C)
-    print("Dịch vòng trái 1 bit của D= ",D)
-    Ks= PC2(C,D)
-    print("Hoán vị PC2: ", Ks)
-
+    K = "03756CD378146EC7"
+    K = hexToBin(K)
+    K0 = PC1(K)
+    print("=> Hoán vị PC1: ",K0, sep="")
+    C0,D0= SPLIT_KEY(K0)
+    print(f"C0 = {C0}")
+    print(f"D0 = {D0}")
+    C1=ShiftLeft(C0, 1)
+    D1=ShiftLeft(D0, 1)
+    print(f"Dịch vòng trái 1 bit của C0 = C1 = {C1}")
+    print(f"Dịch vòng trái 1 bit của D0 = D1 = {D1}")
+    K1 = PC2(C1,D1)
+    print(f"=> Hoán vị PC2(C1D1) = {K1} ")
