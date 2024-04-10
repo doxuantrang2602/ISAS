fcca58297aa959f9901dbccf05b307545860c073
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-10 17:02:19 +0700

    Update Giải mã Vigenere lặp khóa

--- Chương 1 - Mã hóa cổ điển/Giải mã/2. Giải mã Vigenere Lặp khóa.py
+++ Chương 1 - Mã hóa cổ điển/Giải mã/2. Giải mã Vigenere Lặp khóa.py
@@ -1,27 +1,23 @@
-def giaiMaVigenere(C, K):
-    lstAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    Alpha = {char: idx for idx, char in enumerate(lstAlpha)}
-
-    # Xử lý mở rộng K để phù hợp với độ dài của C
-    moRong_K = K * (len(C) // len(K)) + K[:len(C) % len(K)]
-
-    M_giai = []  # Lưu bản rõ
-
-    print("======================================================")
-    print(f'| {"C":<5} | {"K":<5} | {"C index":<8} | {"K index":<8} | {"M index":<8} | {"M"} |')  # In tiêu đề cho bảng
-    print("======================================================")
-
-    for c, k in zip(C, moRong_K):
-        c_index = Alpha[c]  # Chỉ số của kí tự c trong bảng chữ cái
-        k_index = Alpha[k]  # Chỉ số của kí tự k trong bảng chữ cái
-        m_index = (c_index - k_index) % 26  # Tính chỉ số mới cho kí tự giải mã
-        M_giai.append(lstAlpha[m_index])  # Thêm kí tự giải mã vào bản rõ
-        print(f'| {c:<5} | {k:<5} | {c_index:<8} | {k_index:<8} | {m_index:<8} | {lstAlpha[m_index]} |')
-    print("======================================================")
-    print("Bản rõ sau khi giải mã Vigenere lặp khóa là: ", ''.join(M_giai), sep="")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    dicAlpha = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6,
+                'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13,
+                'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
+                'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
     C = "HVZRQFXSMALYKCI"
     K = "WHENIN"
-    giaiMaVigenere(C, K)
+    while len(K) < len(C):
+        K += K
+    K = K[:len(C)]
+    M = ""
+    for c, k in zip(C, K):
+        if c in dicAlpha and k in dicAlpha:
+            c_index = dicAlpha[c]
+            k_index = dicAlpha[k]
+            m_index = (c_index - k_index + 26) % 26
+            for k,v in dicAlpha.items():
+                if v == m_index:
+                    M += k
+                    break
+        else:
+            M += c
+    print(f"Bản rõ M = {M}")
