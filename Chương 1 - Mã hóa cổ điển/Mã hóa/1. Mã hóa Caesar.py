9dc4dc038c8451a54a1b89618ef8e971c418cf86
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-10 15:39:47 +0700

    Update Done new Mã hóa Caesar

--- Chương 1 - Mã hóa cổ điển/Mã hóa/1. Mã hóa Caesar.py
+++ Chương 1 - Mã hóa cổ điển/Mã hóa/1. Mã hóa Caesar.py
@@ -6,20 +6,21 @@ Key: K = 18
 '''
 
 if __name__ == "__main__":
-    lstAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    dicAlpha = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6,
+                'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13,
+                'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
+                'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
     M = "ABADBEGINNINGMAK"
     K = 18
-    Alpha = {}
-    for i in range(len(lstAlpha)):
-        Alpha[lstAlpha[i]] = i
     C = ""
     for m in M:
-        if m in Alpha:
-            p = Alpha[m]
-            c = (p+K) % 26
-            for v, k in Alpha.items():
-                if k == c:
-                    C += v
+        if m in dicAlpha:
+            p = dicAlpha[m]
+            c = (p+K)%26
+            for k,v in dicAlpha.items():
+                if v == c:
+                    C += k
                     break
+        else:
+            C += m
     print(f"Output C = {C}")
