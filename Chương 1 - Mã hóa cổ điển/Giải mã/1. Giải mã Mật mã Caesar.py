b2fed5b9b9a9c5949e24a702d4bed7c0380d1ef9
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-10 16:58:05 +0700

    Update Giải mã Caesar

--- Chương 1 - Mã hóa cổ điển/Giải mã/1. Giải mã Mật mã Caesar.py
+++ Chương 1 - Mã hóa cổ điển/Giải mã/1. Giải mã Mật mã Caesar.py
@@ -1,23 +1,20 @@
-def solveCeasar(C, K):
-    lstAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    Alpha = {}
-    for i in range(0, 26):
-        Alpha[lstAlpha[i]] = i
-    M_giai = []  # Lưu bản rõ
-
-    for c in C.upper():  # Chuyển bản mã C sang chữ hoa
-        if c in Alpha:  # Kiểm tra xem kí tự c có trong bảng chữ cái không
-            p = Alpha[c]  # Tìm vị trí của kí tự c trong bảng chữ cái
-            m = (p - K) % 26  # Tính vị trí mới sau khi đã dịch chuyển ngược K
-            M_giai.append(lstAlpha[m])  # Thêm kí tự giải mã vào bản rõ
-        else:
-            M_giai.append(c)  # Nếu kí tự không phải chữ cái, giữ nguyên
-
-    return ''.join(M_giai)  # Chuyển list kí tự thành chuỗi
-
 if __name__ == "__main__":
+    dicAlpha = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6,
+                'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13,
+                'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
+                'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
     C = "STSVTWYAFFAFYESC"
     K = 18
-    M_res = solveCeasar(C, K)
-    print(f"=> Bản rõ sau khi giải mã C với K = {K} là: {M_res}")
+    M = ""
+    for c in C:
+        if c in dicAlpha:
+            p = dicAlpha[c]
+            m = (p-K)%26
+            for k, v in dicAlpha.items():
+                if v == m:
+                    M += k
+                    break
+        else:
+            M += c
+    print(f"Bản rõ M = {M}")
+
