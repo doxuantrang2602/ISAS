b7b67c9bbcdceeb8bc5b8008cba03b6d779cc0e8
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-10 15:23:03 +0700

    Update Mã hóa Caesar

--- Chương 1 - Mã hóa cổ điển/Mã hóa/1. Mã hóa Caesar.py
+++ Chương 1 - Mã hóa cổ điển/Mã hóa/1. Mã hóa Caesar.py
@@ -7,22 +7,19 @@ Key: K = 18
 
 if __name__ == "__main__":
     lstAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    M = input("Nhập M = ")
-    K = int(input("Nhập K = "))
-    M = list(M.upper()) # Chuyển đổi chuỗi M nhập vào thành 1 list và chuyển sang dạng chữ hoa
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    M = "ABADBEGINNINGMAK"
+    K = 18
     Alpha = {}
-    for i in range(0,26):
-        Alpha[lstAlpha[i]] = i # Cấp phát chỉ số cho mỗi chữ cái 'A':0, 'B':1, ..., 'Z':25)
-    C = [] #Lưu Output C
+    for i in range(len(lstAlpha)):
+        Alpha[lstAlpha[i]] = i
+    C = ""
     for m in M:
-        if m in Alpha: # Kiểm tra xem m có trong bảng chữ cái không
-            p = Alpha[m] # Tìm vị trí p của kí tự đó trong Dic Alpha
-            c = (p+K)%26 # Tính vị trí mới của kí tự sau khi đã dịch chuyển K
-            for v, k in Alpha.items(): # v: key (A->Z), k: value(chỉ số tương ứng)
+        if m in Alpha:
+            p = Alpha[m]
+            c = (p+K) % 26
+            for v, k in Alpha.items():
                 if k == c:
-                    C.append(v) # Thêm kí tự v vào list C
+                    C += v
                     break
-    M = ''.join(M) # Chuyển M lại thành chuỗi
-    print("=> Kết quả của mật mã Ceasar với M = {}, K = {} là: {}".format(K, M, ''.join(C), sep = ""))
-
+    print(f"Output C = {C}")
