bf8873db072a1bfb8a6340e9f6e2eb1c49149417
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-10 16:15:19 +0700

    Update new Vigenere AutoKey

--- Chương 1 - Mã hóa cổ điển/Mã hóa/3. Vigenere - AutoKey.py
+++ Chương 1 - Mã hóa cổ điển/Mã hóa/3. Vigenere - AutoKey.py
@@ -4,26 +4,22 @@ Key: K = WHENIN
 => Tìm Output: C = HVZRQFMZDRLDPGM
 '''
 if __name__ == "__main__":
-    lstAlpha=['A','B','C','D','E','F','G','H','I','J','K','L','M',
-                  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    M= list(input("Nhập M = "))
-    K= list(input("Nhập K = "))
-    Alpha={}
-    for i in range(0,26):
-        Alpha[lstAlpha[i]]=i
-
-    while len(K) < len(M): # Lặp khi độ dài khóa K < M
-        K += M[:len(M) - len(K)] # Mở rộng K bằng cách thêm vào đầu của M cho đến khi len(K) = len(M)
-
-    C = []
-    print("======================================================")
-    print(f'| {"M":<5} | {"K":<5} | {"M index":<8} | {"K index":<8} | {"C index":<8} | {"C"} |')  # In tiêu đề cho bảng
-    print("======================================================")
+    dicAlpha = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6,
+                'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13,
+                'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
+                'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
+    M = "LOVEISBLINDLOVE"
+    K = "WHENIN"
+    while len(K) < len(M):
+        K += M[:len(M)]
+    K = K[:len(M)]
+    C = ""
     for m, k in zip(M, K):
-        c = (Alpha[m] + Alpha[k]) % 26
-        for a, b in Alpha.items():
-            if b == c:
-                C.append(a)
-        print(f'| {m:<5} | {k:<5} | {Alpha[m]:<8} | {Alpha[k]:<8} | {c:<8} | {lstAlpha[c]} |')
-    print("=> Kết của của Vigenere AutoKey: ",''.join(C), sep="")
-
+        m_index = dicAlpha[m]
+        k_index = dicAlpha[k]
+        c_index = (m_index+k_index)%26
+        for k, v in dicAlpha.items():
+            if v == c_index:
+                C += k
+                break
+    print(f"Output C = {C}")
