f9ebca7fdea3e7ad2bedfd7b293e24ad92e35f45
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-10 17:20:25 +0700

    Update Vigenere AutoKey

--- Chương 1 - Mã hóa cổ điển/Giải mã/3. Giải mã Vigenere AutoKey.py
+++ Chương 1 - Mã hóa cổ điển/Giải mã/3. Giải mã Vigenere AutoKey.py
@@ -1,30 +1,29 @@
-def Solve(C, K):
-    lstAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    Alpha = {}
-    for i in range(0, 26):
-        Alpha[lstAlpha[i]] = i
-    M = []
-    while len(K) < len(M):  # Lặp khi độ dài khóa K < M
-        K += M[:len(M) - len(K)]   # Mở rộng K bằng cách thêm vào đầu của M cho đến khi len(K) = len(M)
-    print("============================================================")
-    print(f'| {"C":<5} | {"K":<5} | {"C index":<8} | {"K index":<8} | {"M index":<8} | {"M"} |')
-    print("============================================================")
-    for i, c in enumerate(C):
-        c_index = Alpha[c]
-        if i < len(K):
-            k_index = Alpha[K[i]]
-        else:
-            k_index = Alpha[M[i - len(K)]]
-        m_index = (c_index - k_index) % 26
-        M.append(lstAlpha[m_index])
-        print(f'| {c:<5} | {lstAlpha[k_index]:<5} | {c_index:<8} | {k_index:<8} | {m_index:<8} | {lstAlpha[m_index]} |')
-    return ''.join(M)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    dicAlpha = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6,
+                'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13,
+                'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
+                'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
     C = "HVZRQFMZDRLDPGM"
     K = "WHENIN"
+    M = ""
+    for i in range(len(C)):
+        c = C[i]
+        if c in dicAlpha:
+            if i < len(K):
+                k = K[i]
+            else:
+                k = M[i - len(K)]
+            if k in dicAlpha:
+                c_index = dicAlpha[c]
+                k_index = dicAlpha[k]
+                m_index = (c_index - k_index + 26) % 26
+                for k, v in dicAlpha.items():
+                    if v == m_index:
+                        M += k
+                        break
+            else:
+                M += c
+        else:
+            M += c
+    print(f"Bản rõ M = {M}")
 
-    M_res = Solve(C, K)
-    print("============================================================")
-    print(f"Bản rõ sau khi giải mã Vigenere AutoKey: {M_res}")
