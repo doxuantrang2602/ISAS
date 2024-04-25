d90218ec2c6bbfa1760c795348d61e21c2ddc7cf
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-25 20:36:22 +0700

    Tính phiEuler sử dụng sàng số nguyên tố

--- Chương 2 - Modulo/4. Tính giá trị hàm Euler phi(n).py
+++ Chương 2 - Modulo/4. Tính giá trị hàm Euler phi(n).py
@@ -3,21 +3,17 @@ TÍNH GIÁ TRỊ HÀM EULER phi(n).
 Input: n = 3353
 Tìm Output: phi(n) =
 '''
-import math
+from CacHamBoTro import ptThuaSoNguyenTo
+
 def phiEuler(n):
-    res = n #Khởi tạo phi(n) = n
-    for i in range(2, int(math.sqrt(n)+1)):
-        if n % i == 0:
-            res = res * (1 - 1.0/i) # Giảm phi (n)
-            while n%i == 0:
-                n //= i # ta loại bỏ tất cả thừa số i khỏi n
-    if n > 1:
-        res -= res/n
+    factors = ptThuaSoNguyenTo(n)
+    res = n
+    for p in factors:
+        res *= (1 - 1.0/p)
     return int(res)
 
 if __name__ == "__main__":
-    n = int(input("Nhập n = "))
+    n = 3353
     phi_n = phiEuler(n)
-    print("=> Output phi(n) = {}".format(phi_n))
-
+    print(f"=> Output phi(n) = {phi_n}")
 
