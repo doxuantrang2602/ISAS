a1a468c8e362afa21806101c72e6087dd312a649
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-25 22:09:21 +0700

    Clean code Tính các biểu thức Modulo cơ bản

--- Chương 2 - Modulo/10. Tính các biểu thức Modulo cơ bản.py
+++ Chương 2 - Modulo/10. Tính các biểu thức Modulo cơ bản.py
@@ -9,33 +9,7 @@ Input: a = 43; b = 53; x = 308; y = 455; n = 179
 Tìm Output: A1 = ; A2 = ; A3 = ; A4 = ; A5 = ?
 '''
 
-def tinhLuyThuaModulo(a, m, n):
-    res = 1 # Khởi tạo kết quả
-    a = a % n # Chuyển a thành a mod n
-    while m > 0:
-        if m % 2 == 1:
-            res = (res * a) % n  # Nếu m là số lẻ, nhân b với a
-        m = m // 2 # Hạ bậc lũy thừa m
-        a = (a * a) % n  # Cập nhật a
-    return res
-
-def euclidMoRong(a, n):
-    r1, r2 = n, a
-    x1, x2 = 1, 0
-    y1, y2 = 0, 1
-    while r2 != 0:
-        q = r1 // r2  # Tính thương
-        r1, r2 = r2, r1 - q * r2  # Cập nhật r
-        x1, x2 = x2, x1 - q * x2  # Cập nhật x
-        y1, y2 = y2, y1 - q * y2  # Cập nhật y
-    res = 0
-    if r1 == 1:
-        res = y1
-    else: res = None
-    # Nếu res là số âm
-    if res is not None and res < 0:
-        res += n
-    return res
+from CacHamBoTro import tinhLuyThuaModulo, euclidMoRong
 
 if __name__ == "__main__":
     a, b, x, y, n = 43, 53, 308, 455, 179
@@ -47,8 +21,8 @@ if __name__ == "__main__":
     A4 = euclidMoRong(tinhLuyThuaModulo(b, y, n), n)
     A5 = (tinhLuyThuaModulo(a, x, n) * euclidMoRong(tinhLuyThuaModulo(b, y, n), n)) % n
 
-    print("=> A1 =", A1)
-    print("=> A2 =", A2)
-    print("=> A3 =", A3)
-    print("=> A4 =", A4)
-    print("=> A5 =", A5)
+    print(f"=> A1 = {A1}")
+    print(f"=> A2 = {A2}")
+    print(f"=> A3 = {A3}")
+    print(f"=> A4 = {A4}")
+    print(f"=> A5 = {A5}")
