a860e31ece850f3e77c4e3c898e542041b2f3f8f
Do Xuan Trang <xuantrang260203@gmail.com>
2024-04-25 22:12:12 +0700

    Update new Tính lũy thừa Modulo

--- Chương 2 - Modulo/1. Tính lũy thừa modulo.py
+++ Chương 2 - Modulo/1. Tính lũy thừa modulo.py
@@ -1,7 +1,7 @@
 '''
 1. TÍNH LŨY THỪA MODULO 𝒃 = 𝒂^𝒎 𝒎𝒐𝒅 𝒏 BẰNG CÁCH HẠ BẬC LŨY THỪA
 Input: a = 449; m = 6763; n = 6763
-Tìm Output: b =
+Tìm Output: b = 449
 '''
 
 def tinhLuyThuaModulo(a,m,n):
@@ -10,6 +10,10 @@ def tinhLuyThuaModulo(a,m,n):
     while i <= m:
         D[i] = pow(a, i, n)
         i *= 2
+    print(f"|  {'i':<6} |  {'a^i mod n':<10} |")
+    print(f"--------------------------")
+    for p, v in D.items():
+        print(f"|  {p:<6} |  {v:<10} |")
     res = 1
     for p in sorted(D.keys(), reverse=True):
         if m >= p:
@@ -18,8 +22,8 @@ def tinhLuyThuaModulo(a,m,n):
     return res
 
 if __name__ == "__main__":
-    a = 2602
-    m = 382
-    n = 7001
-    b = tinhLuyThuaModulo(a, m, n)
-    print(f"=> Output b = {b}")
+    a = 449
+    m = 6763
+    n = 6763
+    res = tinhLuyThuaModulo(a,m,n)
+    print(f"=> a^m mod n = {res}")
